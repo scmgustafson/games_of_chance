1b0120124f22efe84af2d9334ad2abf93d2cf16f
unknown <69777608+scmgustafson@users.noreply.github.com>
2020-09-25 09:39:12 -0700

    Added more game logic to card_pick.py and instructions to README.md

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ Cho Han-
 - The dice will roll
 - Win or lose
 - Enter y to replay or n to quit
+
+Card Pick-
+- Start the game by starting card_pick.py
+    - python card_pick.py
+- Enter a bet amount as an integer
+- Enter "card1" or "card2" as a guess
+- Two cards will be drawn from a deck of cards
+- Win or lose
+- Enter y to replay or n to quit
--- card_pick.py
+++ card_pick.py
@@ -9,7 +9,12 @@ def card_draw():
     bet = 0
     guess = None
     play_again = None
-    
+
+    #Get player input for bet and guess
+    bet = int(input("Please enter the amount you wish to bet: "))
+    print("Please try to guess which card will be higher.")
+    guess = str(input("card1 or card2: "))
+
     #This builds the "deck"
     ## suits = ["spades","clubs","hearts","diamonds"]
     faces = {2,3,4,5,6,7,8,9,10,"jack","queen","king","ace"}
@@ -20,36 +25,66 @@ def card_draw():
     
     #This pulls the first card from the deck and "removes" it
     #until deck is "shuffled"
-    card = random.choice(deck)
-    deck.remove(card)
+    card1 = random.choice(deck)
+    deck.remove(card1)
 
     #This pulls card #2
     #Not removing its value from the deck because it does not need to be
     card2 = random.choice(deck)
 
     #Flavor/debugging text
-    print("The first card is a " + str(card))
-    print("The second card is a " + str(card2))
+    print("The first card is a: " + str(card1))
+    print("The second card is a: " + str(card2))
 
     #This takes any "face" card and gives it an equivalent integer value
     #used for comparing the 2 cards
-    if card == "jack" or card == "queen" or card == "king":
-        card = 10
-    elif card == "ace":
-        card = 11
+    if card1 == "jack" or card1 == "queen" or card1 == "king":
+        card1 = 10
+    elif card1 == "ace":
+        card1 = 11
 
     if card2 == "jack" or card2 == "queen" or card2 == "king":
         card2 = 10
     elif card2 == "ace":
         card2 = 11
     
-    #This compares the 2 cards
-    if card > card2:
-        print("card1 is higher")
-    elif card2 > card:
-        print("card2 is higher")
-    elif card == card2:
+    #This compares the 2 cards and awards/takes money
+    if card1 > card2 and guess == "card1":
+        print("The first card is higher.")
+        print("You win!")
+        money = money + bet
+        print("You now have " + str(money) + " money.")
+    elif card2 > card1 and guess == "card2":
+        print("The second card is higher.")
+        print("You win!")
+        money = money + bet
+        print("You now have " + str(money) + " money.")
+    elif card1 == card2:
         print("Draw!")
+        print("Your money has been returned.")
+        print("You now have " + str(money) + " money.")
+    elif card1 > card2 and guess == "card2":
+        print("The first card is higher.")
+        print("You lose!")
+        money = money - bet
+        print("You now have " + str(money) + " money.")
+    elif card2 > card1 and guess == "card1":
+        print("The second card is higher.")
+        print("You lose!")
+        money = money - bet
+        print("You now have " + str(money) + " money.")
+
+    #Play again mechanism
+    play_again = input("Do you want to play again? Y or N: ")
+    if play_again == "y" and money > 0:
+        card_draw()
+    elif play_again == "y" and money <= 0:
+        print("You are out of money.")
+        print("Goodbye!")
+        exit()
+    else:
+        print("Goodbye!")
+        exit()
 
 #Call functions here
 card_draw()
